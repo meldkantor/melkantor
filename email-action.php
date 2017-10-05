c801810baa5197529676d0736c4b7fcbae29ccd7
Melissa Kantor <melissakantor@Mels-MacBook-Pro.local>
2017-10-05 11:11:37 -0700

    initial commit

---
+++ email-action.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Rtve</title>	  
+	<link rel="shortcut icon" href="favicon.ico"/> 
+	  <META HTTP-EQUIV="REFRESH" CONTENT="2;URL=contact_thanks.html">
+	<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
+</head>
+<style type="text/css">
+	.thanks{
+		text-align: center;  color: green;  font-family: cursive;  position: absolute;  top: 30%;  left: 0; font-size: 32px;  margin: auto;  right: 0;  height: 40px;
+	}
+	
+</style>
+<body style="background:gainsboro; height:100vh;">	
+
+	<p class="thanks"> Please Wait <i style="margin-left:40px;" class="fa fa-spinner fa-spin fa-lg"></i> </p>
+
+</body>
+</html>
